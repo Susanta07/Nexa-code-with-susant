e3b53ea1fe346111b7ffa515ddb9418fa06d4c04
Code with Susant <154358595+Susanta07@users.noreply.github.com>
2024-12-13 12:52:56 +0530

    new api update

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const clearChatButton = document.getElementById("deleteButton");
 let currentUserMessage = null;
 let isGeneratingResponse = false;
 
-const GOOGLE_API_KEY = "AIzaSyDYZX48qHyGHcSx8LPe9cufxZfAByyE1Bo";
+const GOOGLE_API_KEY = "AIzaSyB9kxR2EoV3QfekrQIdJyoeP49mpnbJ1JE";
 const API_REQUEST_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${GOOGLE_API_KEY}`;
 
 // Load saved data from local storage
